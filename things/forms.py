6b17751451da7b6ebded6df223cac0507d4cc95b
Sakura Mikano <sakuratmik@gmail.com>
2022-10-25 21:23:25 +0100

    Excercise 1 complete, create form and tests based on model

--- things/forms.py
+++ things/forms.py
@@ -1,4 +1,14 @@
 """Forms of the project."""
+from django import forms
+from .models import Thing
 
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        widget = {'description' : forms.Textarea(), 'quantity' : forms.NumberInput()}
+    # name = forms.CharField(label='name')
+    # description = forms.CharField(label='description', widget=forms.Textarea())
+    # quantity = forms.CharField(label='quantity', widget=forms.NumberInput())
 # Create your forms here.
 #testing push
--- things/tests.py
+++ things/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django import forms
+from things.forms import ThingForm
 
 # Create your tests here.
+class TestThingForm(TestCase):
+    def setUp(self):
+        self.form_input = {'name' : 'Jane', 'description': 'Object which peels potatos', 'quantity': 3}
+
+    def test_form_contains_required_fields(self):
+        form = ThingForm()
+        self.assertIn('name', form.fields)
+        self.assertIn('description', form.fields)
+        self.assertIn('quantity', form.fields)
+
+    def test_form_accepts_valid_input(self):
+        form = ThingForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
+
+    def test_form_rejects_empty_name(self):
+        self.form_input['name'] = ''
+        form = ThingForm(data=self.form_input)
+        self.assertFalse(form.is_valid())
+
+    def test_form_rejects_low_quantity(self):
+        self.form_input['quantity'] = -3
+        form = ThingForm(data=self.form_input)
+        self.assertFalse(form.is_valid())
+
+    def test_form_rejects_high_quantity(self):
+        self.form_input['quantity'] = 333
+        form = ThingForm(data=self.form_input)
+        self.assertFalse(form.is_valid())
